905d45c27aa1106e7a6f3f0281d06b54facfec85
cazzysoci <154034397+cazzysoci@users.noreply.github.com>
2024-01-06 14:32:49 +0800

    Update cazinject.py

--- cazinject.py
+++ cazinject.py
@@ -1,15 +1,17 @@
 import phonenumbers
-from phonenumbers import geocoder
+from phonenumbers import geocoder, carrier
 
 def track_phone_number(phone_number):
     try:
         parsed_number = phonenumbers.parse(phone_number, "PH")
         region = geocoder.description_for_number(parsed_number, "en")
-        return region
+        carrier_name = carrier.name_for_number(parsed_number, "en")
+        return region, carrier_name
     except phonenumbers.phonenumberutil.NumberParseException:
         return "Invalid phone number"
 
-phone_number = "+639266659001"  
+phone_number = "+63"  
 
-location = track_phone_number(phone_number)
+location, network = track_phone_number(phone_number)
 print(f"The location of {phone_number} is: {location}")
+print(f"The network provider for {phone_number} is: {network}")
